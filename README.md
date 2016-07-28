52336224e7e6e5718abb2748c5ece0f3be0edae8
Pixelastic <tim@pixelastic.com>
2016-07-28 11:06:36 +0200

    Adding readme and code

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# done
+# done-did
 
+Command line tool to use with your todo, to keep track of what you
+did.
+
+At Algolia, at the end of each week, we send an email with the important tasks
+that we did in the past week. All this information is then aggregated and sent
+to the whole team so everybody can have an overview of what happened in the
+company.
+
+I always have trouble remembering what I did in a given week so I wrote the
+small tool to help me with this email.
+
+Type `done "Here is what I just did"` (or `done` directly, it will open
+your `$EDITOR`). Then type `did` to see the list of tasks you've done in your
+editor.
+
+I delete the whole content when I'm done, to have a fresh start for the
+next week. All content is saved in `~/.done.txt`.
---
+++ lib/done_did.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+require 'tempfile'
+
+# Add a new task to the list of done tasks
+class DoneDid
+  def initialize(*args)
+    @file = File.expand_path('~/.done.txt')
+    @editor = ENV['EDITOR']
+  end
+
+  # Ask the user to fill the message in his editor
+  def prompt
+    tmp = Tempfile.new('done')
+    begin
+      system("#{@editor} #{tmp.path}")
+      message = File.open(tmp.path).read
+    ensure
+      tmp.close
+      tmp.unlink
+    end
+    message
+  end
+
+  def done(message)
+    message = prompt if message.nil?
+    date = Time.now.strftime('[%Y-%m-%d]:')
+    File.open(@file, 'a') do |file|
+      file.puts "#{date} #{message}"
+    end
+    puts "✔ Done"
+  end
+
+  def did
+    system("#{@editor} #{@file}")
+  end
+end
